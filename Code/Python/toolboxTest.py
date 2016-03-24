ed32232dd628cc70f46ba74722bfe0c95530931e
Robin Amsters <robinamsters@gmail.com>
2016-03-24 15:37:30 +0100

    did some tests on Angle

--- Code/Python/toolboxTest.py
+++ Code/Python/toolboxTest.py
@@ -9,7 +9,19 @@ Script to test different aspects of ECSToolbox
 
 import pyrobotECS
 import ECSToolbox as et
+import time
 
 rob=pyrobotECS.Roomba('/dev/ttyUSB0')
 rob.Control()
-bumpLeft, bumpRight, bumpFront, dropLeft, dropRight = et.getSafetySensors(rob)
+rob.sci.song(3,1,64,16)
+rob.sci.play(3)
+
+t_start = time.time()
+t_end = time.time()+10 # end time at 10 seconds, counting from this declaration
+angle_low = bytes(0)
+angle_high = bytes(0)
+
+while time.time() < t_end:
+    rob.sensors.Angle(angle_low, angle_high, 'radians')
+    print rob.sensors['angle']
+    print rob.sensors['distance']
