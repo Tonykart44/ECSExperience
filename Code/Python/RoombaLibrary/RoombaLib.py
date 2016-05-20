e6ae34843af7efb649d04114dda3e9f4554aa505
Robin Amsters <robinamsters@gmail.com>
2016-05-20 20:44:12 +0200

    Add lidarFSM

--- Code/Python/RoombaLibrary/RoombaLib.py
+++ Code/Python/RoombaLibrary/RoombaLib.py
@@ -3,14 +3,19 @@
 Created on Wed Apr 06 22:16:49 2016
 
 @author: Robin Amsters
+email: robinamsters@gmail.com
 
 This module aimes to combine functionalities from the RoombaSCI and pyrobotECS 
-modules. The goal is to provide one module that can be imported to have the 
-of both libraries, since RoombaSCI provides much better syntax, but pyrobotECS
+modules. The goal is to provide one module that can be imported to have the best
+of both libraries, since RoombaSCI provides much easier syntax, but pyrobotECS
 provides raw encoder values.
 
-This module is still in its very early stages and will still have many bugs, 
+This module is still in its  early stages and will still have bugs (most of which 
+are focussed in the LIDAR class, as this particulary tricky to debug), 
 though many base functionalities have been tested succesfully.
+
+Some documentation (dutch) can be found at: https://ecsrobincederic.wordpress.com/
+
 """
 
 from RoombaSCI import RoombaAPI
@@ -34,11 +39,17 @@ class LIDAR(object):
     def initLidar(self):
         """
             Function that initialises LIDAR communication, very simular to 
-            initLidar.m in old RoombaCommunicationToolbox
+            initLidar.m in old RoombaCommunicationToolbox (MATLAB)
+            
+            INPUTS:
+                None
+            OUTPUTS:
+                None
+            
         """
         port = int(self.initPort) # Port to which to send initialisation opcode
-        targetIP = socket.gethostbyname('localhost')
-        targetAddr = (targetIP,port)
+        targetIP = socket.gethostbyname('localhost')  # Communication is done with a socket on the pi
+        targetAddr = (targetIP,port) # Get target address
         outUdpSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # Socket to communicate with
         outUdpSocket.sendto(self.initOpcode,targetAddr) # send opcode
         outUdpSocket.close() # close socket when done
@@ -49,15 +60,21 @@ class LIDAR(object):
             py_udp_receive.m in old RoombaCommunicationToolbox.
             
             This function should only be called after initLidar
+            
+            INPUTS:
+                None
+            OUTPUTS:
+                udpData = data as received from udp_lidar (present on pi) over 
+                          port 7070.
         """     
-        udpInPort = int(self.localPort)
-        UDPTIMEOUT = 1
-        udpInBufferLength = 6000
+        udpInPort = int(self.localPort) # Port to get data from
+        UDPTIMEOUT = 2 
+        udpInBufferLength = 8000 
 
         localIP = socket.gethostbyname('localhost')
         localAddr = (localIP,udpInPort)
         
-        inUdpSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        inUdpSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # Socket to communicate with
         inUdpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         inUdpSocket.settimeout(UDPTIMEOUT)
         
@@ -79,6 +96,14 @@ class LIDAR(object):
             Function that returns LIDAR measurements that were sent over UDP.
             This function is very simular to getLidarScan.m in the old 
             RoombaCommunicationToolbox
+            
+            INPUTS:
+                t_wait = maximum allowed time to wait on LIDAR measurements [s]
+            
+            OUTPUTS:
+                scan = LIDAR data, arranged in a list of measurements. Each
+                       measurement consists of a distance [m] and an angle at
+                       which this distance was found [rad].
         """
         self.initLidar()
         t_start = time.time()
@@ -87,8 +112,6 @@ class LIDAR(object):
         while time.time() <= t_end and len(scan) < 50: # Make sure no infinite loop is possible
             udpdata = self.udp_receive() #Receiving udp data from LIDAR as string
             udpdata = udpdata[1:len(udpdata)-2] # Remove first 2 and last 2 characters
-            
-#            bytes_str=map(int, udpdata.split(","))
             
             data = [] # Convert to floats
             try:
@@ -125,7 +148,7 @@ class LIDAR(object):
                     angle_int = angle_short.view(np.uint16)
                     dist_int = dist_short.view(np.uint16)
                     
-                    
+                
                     angle_i = float((angle_int >> 1)/64) #  Shift one place and convert to float, like MATLAB code
                     angle_i = math.radians(angle_i) # convert angle to radians
                     angle_i = angle_i - math.floor(angle_i/(2*math.pi))*2*math.pi # Limit angle to 2 pi
@@ -134,7 +157,6 @@ class LIDAR(object):
                     
                     scan.append([dist_i,angle_i])
 
-# TODO: test this part
                 # Check whether received data makes sense
                 # If there are some distance measurements that are 0, chances
                 # are that the measurement is good 
@@ -143,8 +165,8 @@ class LIDAR(object):
                     point = scan[i]
                     dist = point[0]
                     if dist <= 0:
+                        del scan[-1] # remove last element from scan as it is usually wrong
                         return scan
-                        # Could try break as well, but this stops both loops
                     else:
                         i +=1
     
@@ -154,7 +176,30 @@ class LIDAR(object):
                     scan = []
         if len(scan) == 0:
             print "LIDAR scan failed, no measurements were returned."
+
         return scan
+	
+    def getShortestDistance(self, scan):
+        """
+            Function that returns the shortest distance of all measurements
+            in a LIDAR scan.
+            
+            INPUTS:
+                scan = LIDAR measurements as returned by getScan
+            
+            OUTPURS:
+                r_min = minimum range presen in scan (whilst ignoring 0 
+                        measurements)
+        """
+	r_min = 10 #initialize as big number, to be replaced in loop
+	if len(scan) > 0:
+		
+		for point in scan:
+			dist = point[0]
+			if dist >= 0.1 and dist <= r_min: # Ignore zeros
+				r_min = dist
+	
+	return r_min
 
 ###############################################################################
 ################################ SENSOR CLASS #################################
@@ -163,11 +208,12 @@ class LIDAR(object):
 class Sensors(object):
     def __init__(self, Roomba, pauze):
         self.API = Roomba.API # RoombaSCI object
-        self.ECS = Roomba.ECS #Roomba object from pyRobotECS
-        self.lidar = LIDAR() #Lidar sensor
-        self.rightEncoder = self.ECS.sensors.getRightEncoderCounts()
+        self.ECS = Roomba.ECS # Roomba object from pyRobotECS
+        self.lidar = LIDAR() #Lidar object
+        # encoders from pyRobotECS
+        self.rightEncoder = self.ECS.sensors.getRightEncoderCounts() 
         self.leftEncoder = self.ECS.sensors.getLeftEncoderCounts()
-        self.pauze = pauze
+        self.pauze = pauze # Might be unnecessary, unclear at this point in time
 #==============================================================================
     def getBumps(self):
         """
@@ -234,8 +280,8 @@ class Sensors(object):
         INPUTS:
                 self = Sensor object
                 direction = direction in which roomba is driving, valid directions:
-                    - 'FWD' = driving forward in a straight line
-                    - 'BWD' = driving backwards in a straight line
+                    - 'FWD' = driving forward in a straight line or arc
+                    - 'BWD' = driving backwards in a straight line or arc
                     - 'CCW' = turning an angle counter clockwise
                     - 'CW' = turning an angle clockwise
                 
@@ -247,11 +293,11 @@ class Sensors(object):
                 
         """     
         # Roomba specific variables
-        wheelCirc = 0.232 #m
-        b = 0.233 #m
+        wheelCirc = 0.232 # circumference of Roomba wheels [m]
+        b = 0.233 # Wheelbase of Roomba [m]
         ticsPerRev = 500
         ticDist = wheelCirc/ticsPerRev
-        maxtics = 65535
+        maxtics = 65535 # max tics before overflow [m]
         
         # Previous encoder values
         prevRight = self.rightEncoder
@@ -376,7 +422,7 @@ class Sensors(object):
         (in that order)
         
         INPUTS:
-                self = Sensor object
+                none
         OUTPUTS:
                 sensorValues = tuple that contains  the value of the wall, 
                 drop and bump sensors
@@ -404,7 +450,6 @@ class Roomba(object):
         self.ECS.sci.full() # Set to full mode
         self.sensors = Sensors(self, 10)
 
-        
 ################################### DRIVING ###################################
 #==============================================================================
     def drive(self, velocity, radius):  
@@ -424,7 +469,6 @@ class Roomba(object):
         speed for a specified distance.
         
         INPUTS:
-            self = Roomba object
             velocity = velocity in m/s at which the Roomba will drive
             distance = distance in m which the Roomba will drive, negative 
                        distance means the roomba will drive backwards.
@@ -932,6 +976,176 @@ if __name__ == "__main__":
                         startFSM = False
                         roomba.play(3)
                         sys.exit("Code execution stopped.")
+
+    elif  "-FSMLidar" in flags:
+        print "Connecting"
+        try:
+            roomba = Roomba(port,baudrate)
+            roomba.sensors.lidar.initLidar()
+        except:
+            roomba = Roomba(alternativePort, baudrate)
+            roomba.sensors.lidar.initLidar()
+        
+        roomba.loadSong(1, 'BEEP')
+        roomba.loadSong(2, 'TFC')
+        roomba.loadSong(3, 'SW')
+        roomba.play(1) 
+        
+        # Initial parameters
+        speed = 0.2 #[m/s]
+        distance = 1 #[m]
+        followRadius = 750 #[mm]
+        bigRadius = -2 #[m]
+        angle_threshold = math.radians(80) #[rad]
+        t_wait = 2 # Time to wait for lidar measurements
+        state = "start" # Initial state
+        
+        # Start program only after clean button has been pushed.
+        startFSM = False
+        print "Press the 'CLEAN' button to start the FSM."        
+        
+        while not startFSM:
+            if roomba.getClean():
+                startFSM = True
+        
+        # Main FSM loop
+        while startFSM:
+            print "State: ", state
+            bumpLeft, bumpRight  = roomba.sensors.getBumps()
+            wall = roomba.sensors.getWallSensor()
+            drops = roomba.sensors.getDrops()
+            
+            if state == "start":
+
+                roomba.driveStraight(speed, 10)    # Drive forward untill wall is found with bump sensor."
+                roomba.turnAngle(speed/2, -math.pi/2, True) # Turn CW untill wall is found with bump sensor."
+                roomba.driveStraight(speed, 10)    # Drive forward untill wall is found with bump sensor."
+                
+                # Look around till wall if found
+                roomba.API.spin_left()
+                th = 0
+                while not wall and not True in drops and th <= math.pi:
+                    wall = roomba.sensors.getWallSensor()   
+                    drops = roomba.sensors.getDrops()
+                    ds,dth = roomba.sensors.getOdometry('CCW')
+                    th += dth
+                    # Safety check
+                    if True in drops:
+                        state = "stop"
+                    if wall:
+                        tmp, tmp2 = roomba.sensors.getOdometry('CCW')
+                        roomba.driveStraight(speed, -0.05, True)  # Drive backwards away from wall
+                        roomba.turnAngle(speed/2, math.pi/4, True) # Turn CCW untill wall is found with bump sensor."
+                        state = "followwall"
+                
+                if not wall:
+                    state = "stop" # After while stop roomba when no wall is found
+                
+            elif state == "followwall":
+                th = 0
+                dth = 0
+                bumpLeft, bumpRight  = roomba.sensors.getBumps()
+                wall = roomba.sensors.getWallSensor()
+                drops = roomba.sensors.getDrops()
+                tmp1,tmp2 = roomba.sensors.getOdometry("FWD") # Call odometry and dont assign values to clear buffer
+
+                while not bumpRight and not True in drops and abs(th) <= abs(angle_threshold):
+                    bumpLeft, bumpRight = roomba.sensors.getBumps()
+                    wall = roomba.sensors.getWallSensor()
+                    drops = roomba.sensors.getDrops()
+                    ds,dth = roomba.sensors.getOdometry("FWD")
+                    th +=dth
+                    scan = roomba.sensors.lidar.getScan(t_wait) 
+                    r_min = roomba.sensors.lidar.getShortestDistance(scan)
+		
+                    if wall or r_min <= 0.5:
+                       roomba.API.drive(int(speed*1000), followRadius)
+                    
+                    else:
+                       roomba.API.drive(int(speed*1000), -followRadius)
+                    
+                    ds, dth = roomba.sensors.getOdometry("FWD")
+                    
+                if True in drops:
+                    state = "stop"
+
+                else: 
+                    if wall and not bumpRight:
+                        state = "followwall" 
+
+                    elif not bumpRight and abs(th) >= abs(angle_threshold):
+                        state = "findwall"
+                
+                    if bumpRight or bumpLeft:
+                        state = "turnCCW"
+                        
+            elif state == "turnCCW":  
+                roomba.API.spin_left()
+                th = 0
+                wall = roomba.sensors.getWallSensor()
+                bumpLeft, bumpRight = roomba.sensors.getBumps()
+                drops = roomba.sensors.getDrops()
+                
+                while not wall and not True in drops and abs(th) <= math.radians(30):
+                    wall = roomba.sensors.getWallSensor()   
+                    drops = roomba.sensors.getDrops()
+                    ds,dth = roomba.sensors.getOdometry('CCW')
+                    th += dth
+                    
+                # Safety check
+                if True in drops:
+                    state = "stop"
+                elif wall:
+                    state = "followwall"
+                else:
+                    state = "findwall"
+                    
+            elif state == "findwall":                
+                tmp1,tmp2 = roomba.sensors.getOdometry('CCW')
+                roomba.turnAngle(speed/2, math.radians(160))                 
+
+                tmp1,tmp2 = roomba.sensors.getOdometry('FWD')
+                roomba.driveArc(speed, bigRadius, math.pi, True)
+                
+                if bumpRight or bumpLeft:
+                     # Look around till wall if found
+                    roomba.API.spin_left()
+                    th = 0
+                    dth = 0
+                    while not wall and not True in drops and th <= math.radians(110):
+                        wall = roomba.sensors.getWallSensor()   
+                        drops = roomba.sensors.getDrops()
+                        ds,dth = roomba.sensors.getOdometry('CCW')
+                        th += dth
+                    # Safety check
+                    if True in drops:
+                        state = "stop"
+                    if wall:
+                        state = "followwall"
+                            
+                    if not wall:
+                        state = "stop"
+                else:
+                     state = "findwall"  
+                
+                
+            elif state == "stop":
+                # Play song
+                roomba.play(2)
+                roomba.stop()
+                
+                # Pauze untill user input is received
+                pauze = True
+                while pauze:
+                    if roomba.getClean():
+                        print "Restarting FSM"
+                        roomba.play(1)
+                        state = "start"
+                        pauze = False
+                    if roomba.getDock() or roomba.getSpot():
+                        startFSM = False
+                        roomba.play(3)
+                        sys.exit("Code execution stopped.")
   
     elif "-demo" in flags:
         roomba = Roomba(port, baudrate)
@@ -1004,17 +1218,19 @@ if __name__ == "__main__":
         roomba = Roomba(port, baudrate)
         roomba.sensors.lidar.initLidar()
 #        time.sleep(2)
-        t_wait = 1
+        t_wait = 2
         
         t_start = time.time()
-        t_end = t_start + 20
+        t_end = t_start + 10
         
         while time.time() < t_end:
             scan = roomba.sensors.lidar.getScan(t_wait)
-        
             print len(scan), " LIDAR measurements received. "
-#            for measurement in scan:
-#                print measurement     
+            for measurement in scan:
+                print measurement 
+            if len(scan) > 0:
+                r_min = roomba.sensors.lidar.getShortestDistance(scan)
+                print "r_min: ", r_min
         
     elif "-control" in flags: 
         roomba = RoombaAPI(port, baudrate)        
